b94f195c69368e4a6d17dfd1b40baf761827289a
unknown <you@example.com>
2020-04-23 18:10:54 -0400

    Arreglado BootStrap

--- CRUD_publicaciones.php
+++ CRUD_publicaciones.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="CSS/bootstrap.css">
+    <link rel="stylesheet" href="Vista/bootstrap.css">
     <style type="text/css">
         #nuevaConvocatoria:link
         {
